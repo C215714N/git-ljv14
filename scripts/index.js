ce1b0bc6be6bb7cc9226629bef28b62bb1a87424
Nicolas Ruiz <nicolas_8585@hotmail.com>
2022-09-22 19:06:21 -0300

    Contenido de main y carrusel con peliculas, otras correciones

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,29 @@
 // Importacion de archivos
+
+//script para el carrusel de peliculas
+
+let carrusel = document.querySelector('#carrusel');
+
+let maxScrollIzq = carrusel.scrollWidth - carrusel.clientWidth;
+let intervalo;
+let pasos = 1;
+
+const movimiento = () => {
+    intervalo = setInterval(function () {
+        carrusel.scrollLeft = carrusel.scrollLeft + pasos;
+        if (carrusel.scrollLeft === maxScrollIzq) {
+            pasos = pasos * -1;
+        } else if (carrusel.scrollLeft === 0) {
+            pasos = pasos * -1;
+        }
+    }, 15);
+    };
+
+const detenerMovimiento = () => {
+    clearInterval(intervalo);
+};
+
+carrusel.addEventListener('mouseover', detenerMovimiento);
+carrusel.addEventListener('mouseout', movimiento);
+
+movimiento();
